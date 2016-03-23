b62e38b6bb7595c390262fc9c3a7ca3630cedf6d
jor93 <robert.johner@gmail.com>
2016-03-23 15:18:12 +0100

    added db

--- app/src/main/java/com/example/jor/hospital/DBCheck.java
+++ app/src/main/java/com/example/jor/hospital/DBCheck.java
@@ -25,6 +25,7 @@ public class DBCheck extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dbcheck);
+        //as
     }
 
     private void createPatients() {
