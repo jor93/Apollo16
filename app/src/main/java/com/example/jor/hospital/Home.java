29d64ed5a34117c09d75932b28c18d1e01c97d5a
mas18 <sandro.mathier@students.hevs.ch>
2016-03-23 15:39:10 +0100

    asdfasdfkaö

--- app/src/main/java/com/example/jor/hospital/Home.java
+++ app/src/main/java/com/example/jor/hospital/Home.java
@@ -30,6 +30,12 @@ public class Home extends AppCompatActivity {
             doctor = null;
 
 
+
+
+        // Hallo Robi du blöde Fotze
+        // chasch das läsu?
+        // Vertrüüsch mer das wirkli ah?
+
     }
 
 
